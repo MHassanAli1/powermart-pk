18109c54fa849fc32e63ba28e8c8f06abc607ba2
Haziq <haziqimran492@gmail.com>
2025-12-05 02:35:51 +0500

    added the schema for orders

---
+++ powermart-frontend/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ctgxIZyeU/Pcn8cd64BOGreM+wFiC3nZZ23zFtTkWlc=\"\n}"
---
+++ powermart-frontend/.next/dev/static/chunks/_a0ff3932._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_16b05383._.js",
+  "static/chunks/7d3db_next_dist_compiled_react-dom_59d9daf9._.js",
+  "static/chunks/7d3db_next_dist_compiled_react-server-dom-turbopack_efd0c5f8._.js",
+  "static/chunks/7d3db_next_dist_compiled_next-devtools_index_c1c76799.js",
+  "static/chunks/7d3db_next_dist_compiled_d263b7f0._.js",
+  "static/chunks/7d3db_next_dist_client_a3e7060a._.js",
+  "static/chunks/7d3db_next_dist_0dee7baa._.js",
+  "static/chunks/69652_@swc_helpers_cjs_679851cc._.js"
+],
+    source: "entry"
+});
